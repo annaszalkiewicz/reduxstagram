92d60c69511af8691ef94d3a7d909d97baa032fa
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-06-16 23:41:00 +0200

    Create reduxstagram component

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,5 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import './styles/style.styl';
+
+render(<h1>Reduxstagram component!</h1>, document.getElementById('root'));
