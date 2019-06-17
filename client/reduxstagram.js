6703f3c81a829970535ad5e87cb1074a1c47e046
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-06-17 23:20:40 +0200

    Use ReacTDOM

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './styles/style.styl';
 import Main from './components/Main';
 
-render(<Main />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
