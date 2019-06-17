56c3770e63fe3b4c47835291cf0d5ee137d7c01c
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-06-17 23:12:35 +0200

    Create GridPhoto component

---
+++ client/components/GridPhoto.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GridPhoto extends Component {
+  render() {
+    return(
+      <div className="photo-grid">
+        I'm a photo grid!
+      </div>
+    )
+  }
+}
+
+export default GridPhoto;
