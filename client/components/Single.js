f387c0ec34eb8b96df826a599c32ffd8523c1bb6
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-06-17 23:14:20 +0200

    Create Single component

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Single extends Component {
+  render() {
+    return(
+      <div className="photo-grid">
+        I'm a single photo!
+      </div>
+    )
+  }
+}
+
+export default Single;
