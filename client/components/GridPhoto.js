6db10d6e2a1efe480a71b8cfc09bb65eb4184f74
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-06-20 13:20:08 +0200

    Create Photo component

--- client/components/GridPhoto.js
+++ client/components/GridPhoto.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
 
 class GridPhoto extends Component {
   render() {
+    const { posts } = this.props;
     return(
       <div className="photo-grid">
-        I'm a photo grid!
+        {posts.map((post, i) => <Photo 
+          {...this.props} 
+          key={i} 
+          post={post} 
+          i={i} /> )}
       </div>
     )
   }
