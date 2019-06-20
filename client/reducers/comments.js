c37e6cd8b3cc9b664db541fae103d25664bc3579
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-06-20 22:48:23 +0200

    Add postComments and fix adding commnets

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,26 @@
+const postComments = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_COMMENT':         
+      return [...state, {
+          user: action.author,
+          text: action.comment
+      }];
+      
+    case 'REMOVE_COMMENT':
+      return state;
+  
+    default:
+      return state;
+  }
+}
+
 const comments = (state = [], action) => {
-  console.log(state, action);
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
